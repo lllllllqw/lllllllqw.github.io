d6eed9f6c5213a04ab1a3aecacc042d8f21f90c5
lqw <465633856@qq.com>
2018-02-03 16:43:09 +0800

    修改一下

--- src/assets/epcComps/package/dms-photo/index.js
+++
@@ -1,7 +0,0 @@
-import DmsPhoto from "./dms-photo.vue"
-
-DmsPhoto.install = function(Vue) {
-	Vue.component(DmsPhoto.name,DmsPhoto)
-}
-
-export default DmsPhoto
--- src/assets/epcComps/package/dms-upload/index.js
+++
@@ -1,7 +0,0 @@
-import DmsUpload from "./dms-upload.vue"
-
-DmsUpload.install = function(Vue) {
-	Vue.component(DmsUpload.name,DmsUpload)
-}
-
-export default DmsUpload
