2a07311d6667aa5bfeee76c050964df57d4bd0c9
lllllllqw <465633856@qq.com>
2018-03-11 23:19:12 +0800

    新增:babel,promise版sleep

--- speedJavascript/sleep.js
+++ speedJavascript/sleep.js
@@ -6,5 +6,28 @@ function forLoopSleep(sleepTime) {
 }
 
 console.time('forLoopSleep')
-forLoopSleep(3000)
+forLoopSleep(1000)
 console.timeEnd('forLoopSleep')
+
+// 2.Promise版,不占用cpu,但是如果需要跳出,会很麻烦
+function promiseSleep(sleepTime) {
+  return new Promise(resolve => setTimeout( resolve, sleepTime))
+}
+
+console.time('promiseSleep')
+forLoopSleep(1000)
+console.timeEnd('promiseSleep')
+
+
+// function sleep(delay) {
+//   return new Promise(reslove => {
+//     setTimeout(reslove, delay)
+//   })
+// }
+
+// (async function test() {
+//   const t1 = +new Date()
+//   await sleep(1000)
+//   const t2 = +new Date()
+//   console.log(t2 - t1)
+// })()
