a3456bb5a3646886763909098de355cd0fc635fa
lllllllqw <465633856@qq.com>
2018-04-01 22:09:05 +0800

    新增:ES6装饰器

--- nodejs/es6/decorator.js
+++ nodejs/es6/decorator.js
@@ -1,33 +1,61 @@
-function autoCache(callback) {
+// function autoCache(callback) {
+//   let cache = null
+//   return function(...args) {
+//     console.log(args)
+//     if(cache) {
+//       return cache
+//     }else{
+//       cache = callback.apply(this || {}, args)
+//       return cache
+//     }
+//   }
+// }
+
+// const big = autoCache(() => {
+//   console.log('calculate')
+//   return Math.pow(3, 12)
+// })
+
+// const getData = autoCache(() => {
+//   console.log('fetch')
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve.bind(null, 'result')
+//     }, 1000)
+//   })
+// })
+
+
+// console.log(big(123, 5, 8))
+// console.log(big())
+
+// getData().then(res => console.log(res))
+// getData().then(res => console.log(res))
+
+function autoCache(target, name, descriptor) {
+  var oldValue = descriptor.value
   let cache = null
-  return function(...args) {
-    console.log(args)
+  descriptor.value = function(...args) {
     if(cache) {
       return cache
-    }else{
-      cache = callback.apply(this || {}, args)
+    } else {
+      cache = oldValue.apply(this || {}, args)
       return cache
     }
   }
+
+  return descriptor
 }
 
-const big = autoCache(() => {
-  console.log('calculate')
-  return Math.pow(3, 12)
-})
+class MyClass {
+  @autoCache getMax() {
+    console.log('缓存')
+    return Math.max(3, 12)
+  }
+}
 
-const getData = autoCache(() => {
-  console.log('fetch')
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve.bind(null, 'result')
-    }, 1000)
-  })
-})
+let myClass = new MyClass()
 
+console.log(myClass.getMax())
+console.log(myClass.getMax())
 
-console.log(big(123, 5, 8))
-console.log(big())
-
-getData().then(res => console.log(res))
-getData().then(res => console.log(res))
