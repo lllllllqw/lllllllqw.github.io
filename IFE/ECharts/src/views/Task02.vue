f71b1d279f9cf1a510ee4e1b25189269e0a0163d
lllllllqw <465633856@qq.com>
2018-04-06 10:33:07 +0800

    新增:Task02

--- IFE/ECharts/src/views/Task02.vue
+++ IFE/ECharts/src/views/Task02.vue
@@ -3,7 +3,39 @@
 </template>
 
 <script>
+import { codeSubmission, bugResolve } from '../../static/e-charts-data'
 export default {
-
+  data() {
+    return {
+      charts: null
+    }
+  },
+  methods: {
+    initEcharts() {
+      this.charts = this.$echarts.init(this.$refs['e-charts'])
+      this.charts.setOption({
+        title: {
+          text: '实现自定义的统计图表'
+        },
+        tooltip: {},
+        xAxis: {
+          data: bugResolve.map(val => val.date)
+        },
+        yAxis: {},
+        series: [{
+          name: 'Bug解决数量',
+          type: 'bar',
+          data: bugResolve.map(val => val.val)
+        }, {
+          name: '代码提交行数',
+          type: 'line',
+          data: codeSubmission.map(val => val.val)
+        }]
+      })
+    }
+  },
+  mounted() {
+    this.initEcharts()
+  }
 }
 </script>
