29e8f3d930a3036772e9a73188963fe4a3e28a1b
lllllllqw <465633856@qq.com>
2018-08-06 22:25:00 +0800

    feat:新增使用Map对象实现DFA算法敏感词过滤(console?)

---
+++ js/speedJavascript/utils/checkSensitiveWord.js
@@ -0,0 +1,68 @@
+// 创建敏感词 map
+function makeSensitiveMap(wordList) {
+  const map = new Map()
+  for (const word of wordList) {
+    let m = map;
+    for (const char of word) {
+      // 如果没有有该字符的 key, 创建 key
+      if (!m.get(char)) {
+        const next = new Map()
+        // 默认设置下一级为末尾
+        next.set('laster', true)
+        // 将本级设为末尾
+        m.set('laster', false)
+        m.set(char, next)
+      }
+      m = m.get(char)
+    }
+  }
+  return map
+}
+
+// 检查文字从某一索引起是否是敏感词
+function checkSensitiveWord(text, sensitiveMap, index) {
+  let word = ''
+  let sensitive = false
+  let m = sensitiveMap
+  for (let i = index, length = text.length; i < length; i++) {
+    const char = text[i]
+    m = m.get(char)
+    if (m) {
+      word += char
+      if (m.get('laster') === true) {
+        sensitive = true
+      }
+    } else {
+      break
+    }
+  }
+
+  // 俩字成词, 意义何在
+  // if(word.length < 2) {
+  //   sensitive = false
+  // }
+
+  return {
+    sensitive,
+    word
+  }
+}
+
+// 查找一段文字的所有敏感词
+function filterSensitiveWord(text, sensitiveMap) {
+  // 只取英文大小写,数字,以及中文
+  const trimText = text.replace(/\u4e00-\u9fa5|a-z|A-Z|0-9/, '')
+  // 逐个下标检查是否存在敏感词
+  for(let i = 0, length = trimText.length; i < length; i++) {
+    const res = checkSensitiveWord(trimText, sensitiveMap, i)
+    if(res.sensitive) {
+      // 仅仅是 console ,并未过滤
+      // TODO: 如何实现过滤
+      console.log('发现敏感词:', res.word)
+    }
+  }
+}
+
+const sensitiveMap = makeSensitiveMap(['沃日', '敲里吗'])
+
+filterSensitiveWord('我敲里吗!!!沃日', sensitiveMap)
