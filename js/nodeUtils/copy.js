6bdd7c2781ba13305446e8e448b9c842ab9f4a2c
lllllllqw <465633856@qq.com>
2018-09-21 17:52:40 +0800

    feat: add copyWithStream

--- js/nodeUtils/copy.js
+++ js/nodeUtils/copy.js
@@ -4,8 +4,13 @@ function main(from, to) {
   copy(from, to)
 }
 
-function copy(from, to) {
+function copyWithSync(from, to) {
   fs.writeFileSync(to, fs.readFileSync(from))
 }
+
+function copyWithStream(from, to) {
+  fs.createReadStream(from).pipe(fs.createWriteStream(to))
+}
+
 const [, ,from, to] = process.argv
-copy(from, to)
+copyWithStream(from, to)
